10785abe8ddcd62e7ffac678ed5ed41a716224b5
MuriloSilva7 <paulomerola26@gmail.com>
2026-04-02 15:53:46 -0300

    fragments

--- app/src/main/java/com/example/orbis/LoginActivity.java
+++ app/src/main/java/com/example/orbis/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.orbis;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,18 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnLogar;
+
+        btnLogar = findViewById(R.id.btnLogar);
+
+        btnLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
