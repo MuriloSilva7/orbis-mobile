4419ea9878f22d9892dde726fdd3941ff2a581d0
MuriloSilva7 <paulomerola26@gmail.com>
2026-05-05 17:17:01 -0300

    inicio tela sensores

--- app/src/main/java/com/example/orbis/ui/fragments/SensoresFragment.java
+++ app/src/main/java/com/example/orbis/ui/fragments/SensoresFragment.java
@@ -3,64 +3,84 @@ package com.example.orbis.ui.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.orbis.R;
+import com.example.orbis.adapter.MaquinaAdapter;
+import com.example.orbis.adapter.SensorAdapter;
+import com.example.orbis.api.OrbisApiService;
+import com.example.orbis.model.Maquina;
+import com.example.orbis.model.Sensor;
+import com.example.orbis.network.RetrofitClient;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SensoresFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class SensoresFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private RecyclerView recyclerSensores;
+    private SensorAdapter adapter;
+    private List<Sensor> listaSensores;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
 
-    public SensoresFragment() {
-        // Required empty public constructor
+        View view = inflater.inflate(R.layout.fragment_sensores, container, false);
+
+        recyclerSensores = view.findViewById(R.id.recyclerSensores);
+
+        recyclerSensores.setLayoutManager(
+                new LinearLayoutManager(getContext())
+        );
+
+        listaSensores = new ArrayList<>();
+
+        adapter = new SensorAdapter(listaSensores);
+        recyclerSensores.setAdapter(adapter);
+
+        carregarSensores();
+
+        return view;
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SensoresFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SensoresFragment newInstance(String param1, String param2) {
-        SensoresFragment fragment = new SensoresFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private void carregarSensores() {
+        OrbisApiService apiService = RetrofitClient
+                .getInstance()
+                .getApi();
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+        Call<List<Sensor>> call = apiService.getSensores();
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sensores, container, false);
+        call.enqueue(new Callback<List<Sensor>>() {
+            @Override
+            public void onResponse(Call<List<Sensor>> call,
+                                   Response<List<Sensor>> response) {
+
+                if (response.isSuccessful() && response.body() != null) {
+
+                    listaSensores.clear();
+                    listaSensores.addAll(response.body());
+
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Sensor>> call,
+                                  Throwable t) {
+
+                Log.e("ERRO_API", t.getMessage());
+            }
+        });
     }
 }
